d325756b42b236a45ee496c65b6f9aa2a729becb
icortajarena <icortajarena@gukere.com>
2021-09-15 09:17:53 +0200

    initial commit

---
+++ dojo.js
@@ -0,0 +1,3 @@
+export const dojo = () => {
+  return true;
+};
---
+++ dojo.test.js
@@ -0,0 +1,7 @@
+import { dojo } from './dojo';
+
+describe('DOJO', () => {
+  it('Funciona', () => {
+    expect(dojo()).toBe(true);
+  });
+});
