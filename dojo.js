1608e2b263d02e0a755a1d79a3c7a66032584023
Jon Amutxastegi <j.amutxastegi@ibermatica.com>
2021-09-22 16:03:50 +0200

    feat: cuando recibe numero negativo devuelve no existen numeros negativos en romano

--- dojo.js
+++ dojo.js
@@ -1,3 +1,6 @@
 export const numerosRomanos = (numeroArabe) => {
+  if(numeroArabe<0){
+    return "no existen numeros negativos en romano";
+  }
   return "";
 };
--- dojo.test.js
+++ dojo.test.js
@@ -6,4 +6,7 @@ describe('conversor numero arabes a romanos', () => {
   it('Cuando recibe el numero 0 devuelve ""', () => {
     expect(numerosRomanos(0)).toBe("");
   });
+  it ('cuando recibe numeros negativos devuelve "no existen numeros negativos en romano"',()=>{
+    expect(numerosRomanos(-1)).toBe("no existen numeros negativos en romano");
+  })
 });
