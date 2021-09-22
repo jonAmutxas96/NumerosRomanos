aa03210f25cb9b29ea37c84b8e6bb7e1917c6d55
Jon Amutxastegi <j.amutxastegi@ibermatica.com>
2021-09-22 16:54:40 +0200

    fix: si el numero es cero informa del errror

--- dojo.js
+++ dojo.js
@@ -2,5 +2,6 @@ export const numerosRomanos = (numeroArabe) => {
   if(numeroArabe<0){
     return "no existen numeros negativos en romano";
   }
-  return "";
+
+  return "no existe el número 0 en números romanos";
 };
--- dojo.test.js
+++ dojo.test.js
@@ -3,10 +3,11 @@ import { numerosRomanos } from './dojo';
 // Kata número romanos: Convertir números árabes desde 1 hasta 3000 a romanos
 
 describe('conversor numero arabes a romanos', () => {
-  it('Cuando recibe el numero 0 devuelve ""', () => {
-    expect(numerosRomanos(0)).toBe("");
+  it('si el número es 0, informa de que no existe', () => {
+    expect(numerosRomanos(0)).toBe("no existe el número 0 en números romanos");
   });
-  it ('cuando recibe numeros negativos devuelve "no existen numeros negativos en romano"',()=>{
+
+  it ('si el número es negativo informa al usuario',()=>{
     expect(numerosRomanos(-1)).toBe("no existen numeros negativos en romano");
   })
 });
