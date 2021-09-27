883062d4c8580f1144916ec7f74416d46445e0d0
Jon Amutxastegi <j.amutxastegi@ibermatica.com>
2021-09-27 08:54:07 +0200

    refactor: change if structure to use loop

--- dojo.js
+++ dojo.js
@@ -1,57 +1,37 @@
 export const numerosRomanos = (numeroArabe) => {
+  const simboloRomano = ['I', 'V', 'X', 'L', 'C','D', 'M'];
+  const valorSimboloRomano=[1,5,10,50,100,500,1000];
+  let resultadoNumeroRomano='';
   if(numeroArabe<0){
     return "no existen numeros negativos en romano";
+  }
+  if(numeroArabe==0){
+      return "no existe el número 0 en números romanos";
+    
   }
 
   if(numeroArabe > 3888) {
     return "no existen numeros superiores a 3888 en romano"
-  }
-
-  if (numeroArabe === 1) {
-    return 'I';
-  }
-
-  if (numeroArabe === 2) {
-    return 'II';
-  }
-
-  if (numeroArabe === 5) {
-    return 'V';
-  }
-
-  if (numeroArabe === 10) {
-    return 'X';
-  }
-  if (numeroArabe === 30) {
-    return 'XXX';
-  }
+  } 
   if (numeroArabe === 40) {
     return 'XL';
-  }
-
-  if (numeroArabe === 50) {
-    return 'L';
-  }
-
-  if (numeroArabe === 100) {
-    return 'C';
-  }
-
-  if (numeroArabe === 500) {
-    return 'D';
-  }
-
-  if (numeroArabe === 1000) {
-    return 'M';
-  }
-
-  if (numeroArabe === 3) {
-    return 'III'
   }
 
   if (numeroArabe === 4) {
     return 'IV'
   }
 
-  return "no existe el número 0 en números romanos";
+  let indice = 6;
+  while(numeroArabe >0){
+    if (numeroArabe >= valorSimboloRomano[indice]){
+      resultadoNumeroRomano+= simboloRomano[indice];
+      numeroArabe-= valorSimboloRomano[indice];
+    }else{
+    indice--;
+    }
+  }
+
+ 
+
+  return resultadoNumeroRomano;
 };
